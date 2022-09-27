a8af8c4ba67060e750c3e29bbf43b7cd76a9c496
paul_Lee <1337998606@qq.com>
2022-09-27 20:31:39 +0800

    Update backend

---
+++ KOB_backend/src/main/java/com/kob/KobBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KobBackendApplication.class, args);
+    }
+
+}
